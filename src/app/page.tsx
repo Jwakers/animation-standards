15c9260b8ab51450c6eaab9f557e4cb1a65ad277
Jwakers <jackwakeham82@gmail.com>
2023-12-20 16:17:15 +0000

    Heading animations

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,78 @@
 "use client";
 
 import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useGSAP } from "@gsap/react";
-import { useRef, useState, useEffect } from "react";
+import { useRef, useState } from "react";
 import Container from "@/components/container";
 import Syntax from "@/components/syntax";
+import clsx from "clsx";
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
   const headingRef = useRef<HTMLHeadingElement>(null);
+  const containerRef = useRef<HTMLDivElement>(null);
+  const { contextSafe } = useGSAP({ scope: containerRef });
+  const title = "Animation standards";
 
-  useGSAP(() => {
-    gsap.from(headingRef.current, {
-      opacity: 0,
-      scale: 1.2,
+  useGSAP(
+    () => {
+      gsap.to(".letter", {
+        opacity: 1,
+        scale: 1,
+        stagger: 0.04,
+        y: 0,
+      });
+
+      gsap.to(".letter", {
+        scrollTrigger: {
+          trigger: headingRef.current,
+          start: "top 20%",
+          end: "bottom top",
+          scrub: 0.5,
+        },
+        scale: 0,
+        x: "-40",
+        stagger: 0.1,
+      });
+    },
+    { scope: containerRef }
+  );
+
+  const handleMouseEnter = contextSafe((e) => {
+    gsap.to(e.target, {
+      rotateY: 180,
     });
-  }, []);
+  });
+
+  const handleMouseLeave = contextSafe((e) => {
+    gsap.to(e.target, {
+      rotateY: 0,
+    });
+  });
 
   return (
     <main className="min-h-screen">
-      <div className="relative h-screen flex flex-col justify-center items-center">
+      <div
+        ref={containerRef}
+        className="relative h-screen flex flex-col justify-center items-center"
+      >
         <Container>
           <h1 className="text-8xl font-medium" ref={headingRef}>
-            Animation Standards
+            {title.split("").map((letter, i) => (
+              <span
+                key={i}
+                className={clsx(
+                  "letter inline-block opacity-0 -translate-y-10",
+                  letter === " " && "w-6"
+                )}
+                onMouseEnter={handleMouseEnter}
+                onMouseLeave={handleMouseLeave}
+              >
+                {letter}
+              </span>
+            ))}
           </h1>
         </Container>
       </div>
